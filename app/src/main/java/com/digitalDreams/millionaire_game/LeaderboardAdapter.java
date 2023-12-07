d23777cf87cd5e787455e66aff2c021f8d8787cd
Toochidennis <dennistoochukwu@gmail.com>
2023-12-07 09:46:29 +0100

    Update LeaderboardAdapter

--- app/src/main/java/com/digitalDreams/millionaire_game/LeaderboardAdapter.java
+++ app/src/main/java/com/digitalDreams/millionaire_game/LeaderboardAdapter.java
@@ -31,14 +31,14 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
     public LeaderBoardVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         View view;
-                view = layoutInflater.inflate(R.layout.leader_score_item, parent, false);
-                return new LeaderBoardVH(view);
+        view = layoutInflater.inflate(R.layout.leader_score_item, parent, false);
+        return new LeaderBoardVH(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull LeaderBoardVH holder, int position) {
         PlayerObject obj = list.get(position);
-        int newPosition = Integer.parseInt( obj.getPosition()) + 1;
+        int newPosition = Integer.parseInt(obj.getPosition()) + 1;
 
         String rank = String.valueOf(newPosition);
 
@@ -47,28 +47,31 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
         String highscore = obj.getHighscore();
         String country = obj.getCountry();
         String country_flag = obj.getCountry_flag();
-       if(!country_flag.isEmpty() && country_flag.length() > 4){
-           SVGLoader.fetchSvg(context,country_flag, holder.flag_img);
+        if (!country_flag.isEmpty() && country_flag.length() > 4) {
+            try {
+                SVGLoader.fetchSvg(context, country_flag, holder.flag_img);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
+            holder.flag_img.setVisibility(View.VISIBLE);
 
-           holder.flag_img.setVisibility(View.VISIBLE);
+            holder.flag_img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    gotToCountry(country, country_flag);
+                }
+            });
 
-          holder.flag_img.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                  gotToCountry(country,country_flag);
-              }
-          });
-
-       }
+        }
 
         /////
-        holder.countryTxt.setText(Html.fromHtml("<u>"+country+"</u>"));
+        holder.countryTxt.setText(Html.fromHtml("<u>" + country + "</u>"));
         holder.countryTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                gotToCountry(country,country_flag);
+                gotToCountry(country, country_flag);
 
             }
         });
@@ -86,9 +89,8 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
     }
 
 
-
-    class LeaderBoardVH extends RecyclerView.ViewHolder{
-        TextView positionTXT,playerNameTxt,playerScoreTxt,countryTxt;
+    class LeaderBoardVH extends RecyclerView.ViewHolder {
+        TextView positionTXT, playerNameTxt, playerScoreTxt, countryTxt;
         ImageView flag_img;
 
         public LeaderBoardVH(@NonNull View itemView) {
@@ -97,14 +99,14 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
             playerNameTxt = itemView.findViewById(R.id.player_name);
             playerScoreTxt = itemView.findViewById(R.id.highscore);
             countryTxt = itemView.findViewById(R.id.country);
-            flag_img =  itemView.findViewById(R.id.flag_img);
+            flag_img = itemView.findViewById(R.id.flag_img);
             flag_img.setVisibility(View.GONE);
         }
     }
 
-    public  void gotToCountry(String country_name, String country_flag){
+    public void gotToCountry(String country_name, String country_flag) {
 
-        Intent intent = new Intent(context,CountryLeaderBoard.class);
+        Intent intent = new Intent(context, CountryLeaderBoard.class);
         ///Utils.darkBlueBlink(convertView,getApplicationContext());
 
 
@@ -112,11 +114,10 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
 ///                   map.put("country",country);
 ///                   map.put("country_flag",country_flag);
         //intent.putExtra("country_map", (Serializable) map);
-        intent.putExtra("country",country_name);
-        intent.putExtra("country_flag",country_flag);
+        intent.putExtra("country", country_name);
+        intent.putExtra("country_flag", country_flag);
         context.startActivity(intent);
     }
-
 
 
 }
